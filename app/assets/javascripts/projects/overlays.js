c72b1075bd162273dcad9d1540c946ba66a756ca
dbcjckyee <dbcjckyee@gmail.com>
2016-01-18 15:34:25 -0800

    add overlays to project banners

---
+++ app/assets/javascripts/projects/overlays.js
@@ -0,0 +1,22 @@
+addOverlay = function(image){
+  $("#overlay").width(image.css("width"));
+  $("#overlay").height(image.css("height"));
+  $("#overlay").css("top", image.offset().top + "px");
+  $("#overlay").css("left", image.offset().left + "px");
+  $("#overlay").html('<div class = "imagelabel">' + image.attr('id') + '</div>');
+}
+
+removeOverlay = function(){
+  $("#overlay").width(0);
+  $("#overlay").height(0);
+  $("#overlay").html('');
+}
+
+$(document).ready(function(){
+  $('.project').mouseenter(function(){
+    addOverlay($(this));
+  });
+  $('#overlay').mouseleave(function(){
+    removeOverlay();
+  });
+})
