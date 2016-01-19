18e41dc8a40092232590280c3935ad55d701893e
dbcjckyee <dbcjckyee@gmail.com>
2016-01-18 23:30:25 -0800

    add write-ups for all the projects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require_tree .
+//= require_directory .
 
 $(document).ready(function(){
   $('li a').click(function(event){
