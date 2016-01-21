ff6f2bdc5f462e3daffcd144190f62b6712d1e8c
dbcjckyee <dbcjckyee@gmail.com>
2016-01-21 14:04:35 -0800

    add resume download link in controller

---
+++ public/assets/projects/overlays-190f0f689772bd5e3a42b9f8369ede8469dde638f69f4062d43e9f68ffe9f251.js
@@ -0,0 +1,33 @@
+addOverlay = function(image){
+  $("#overlay").width(image.css("width"));
+  $("#overlay").height(image.css("height"));
+  $("#overlay").css("top", image.offset().top + "px");
+  $("#overlay").css("left", image.offset().left + "px");
+  $("#overlay").html('<div class = "imagelabel">' + image.attr('id') + '</div>');
+}
+
+removeOverlay = function(){
+  $("#overlay").width(0);
+  $("#overlay").height(0);
+  $("#overlay").html('');
+}
+
+$(document).ready(function(){
+  $('.project').mouseenter(function(){
+    addOverlay($(this));
+  });
+
+  $('#overlay').mouseleave(function(){
+    removeOverlay();
+  });
+
+  $('#overlay').click(function(){
+    $.ajax({
+      data: {key: $(this).text()},
+      url: '/website/projects/show'
+    })
+    .done(function(result){
+      $('#projecttext').html(result['partial'])
+    })
+  })
+})
